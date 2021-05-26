562b0d37b4ed224dccec774023f5038d3b2fe9b5
Roydano <roy.dano90@gmail.com>
2021-05-26 16:20:38 +0200

    add bonus + correzione su restituzione dati

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 
 // lista variabili richieste
+
+
 var nome, cognome, colore, annoCorrente;
+
 
 
 // richiesta nome
 var nome = prompt ("Inserisci il tuo nome");
 console.log ( nome );
 
+// restituzione nome in minuscolo
+var lowNome = nome.toLowerCase();
+
 // richiesta cognome
 var cognome = prompt ("Inserisci il tuo cognome");
 console.log ( cognome );
+
+// restituzione cognome in minuscolo
+var lowCognome = cognome.toLowerCase();
 
 // richiesta colore preferito
 var colore = prompt ("Inserisci il tuo colore preferito");
 console.log ( colore );
 
+// restituzione colore in minuscolo
+var lowColore = colore.toLowerCase();
+
 // richiesta ultime due cifre dell'anno in corso
 var annoCorrente = parseInt (prompt ("Inserisci le ultime due cifre dell'anno in corso"));
 console.log ( annoCorrente );
 
-var password = document.getElementById("password").innerHTML = "Questa è la tua password: " + nome + cognome + colore + annoCorrente;
+// restituzione dei dati inseriti sulla pagina
+document.getElementById("password").innerHTML = "Questa è la tua password: " + lowNome + lowCognome + lowColore + annoCorrente;
+
+
+
 
 
